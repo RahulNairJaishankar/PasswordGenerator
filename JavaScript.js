7ee01c404f79f1d76288f133b7194d8809dfdff6
RahulJaishankar <rjaisha@purdue.edu>
2019-11-05 23:04:41 -0500

    Added HTML Code and modified parser()
    parser() can now talk to MainPage.html
    Just making a comment over here.

--- JavaScript.js
+++ JavaScript.js
@@ -5,23 +5,32 @@
  */
 function parser(){
 
+
     //declaring elements variables to store elements from "form1"
     let elements = document.getElementById("form1");
 
     //assigning values to variables from elements
-    let DOB = elements[0].value; //not sure about this one yet. Check how date is returned from html form.
-    let name = elements[1].value;
-    let firstPetName = elements[2].value;
-    let bestFriendsName = elements[3].value;
-    let intendedWebsite = elements[4].value;
-    let streetName = elements[5].value;
-
-
+ 
+    let DOB = document.getElementsByName("bday")[0].value; //retuns in YYYY-MM-05
+    let firstPetName = document.getElementsByName("petName")[0].value;
+    let bestFriendsName = document.getElementsByName("friendName")[0].value;
+    let intendedWebsite = document.getElementsByName("intendedSiteName")[0].value;
+    let streetName = document.getElementsByName("streetName")[0].value;
     //TODO data validation
 
     //Checking if user checked any of the check-boxes
     //IMP please change to appropriate ID tags; rn only placeholders
-    leet = document.getElementById("leet").checked;
-    symbols = document.getElementById("symbols").checked;
-    cameCase = document.getElementById("camelCase").checked;
+    let leet = document.getElementsByName("numAsLetters")[0].checked;
+    let symbols = document.getElementsByName("addSymbols")[0].checked;
+    let camelCase = document.getElementsByName("useCamelCase")[0].checked;
+
+    //testing parser functionality
+    console.log(DOB);
+    console.log(firstPetName);
+    console.log(bestFriendsName);
+    console.log(intendedWebsite);
+    console.log(streetName);
+    console.log("num as letters: " + leet);
+    console.log("add symbolsclass: " + symbols);
+    console.log("camelCase: " + camelCase);
 }
